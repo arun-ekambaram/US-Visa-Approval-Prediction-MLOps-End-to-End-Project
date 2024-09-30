940e112b3d9be83f3c987b125d9db0a08d23767f
arun ekambaram <arunekambaram173@gmail.com>
2024-09-30 11:30:03 +0530

    Added Data Validation Component

--- demo.py
+++ demo.py
@@ -2,7 +2,11 @@ from us_visa.logger import logging
 from us_visa.exception import USvisaException
 import sys
 logging.info("Custom Log")
+from us_visa.pipline.training_pipeline import TrainPipeline
 
+
+obj = TrainPipeline()
+obj.run_pipeline()
 # try:
 #     a= 2/0
 # except Exception as e:
